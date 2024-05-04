4a6c2d555a6493027f9574c03972eb52b544f1d4
N-Il <47373830+n-Il@users.noreply.github.com>
2024-05-04 17:14:29 -0400

    fixes to image onclicks

--- web_result_script.js
+++ web_result_script.js
@@ -1,7 +1,7 @@
 //this code creates all the variables we use
-var pageSize = 50;
-var resultStartIndex = 0;
-var resultEndIndex = Math.min(pageSize,results.length);
+let pageSize = 50;
+let resultStartIndex = 0;
+let resultEndIndex = Math.min(pageSize,results.length);
 //this code sets up the text at the top
 document.getElementById("query").innerText = "using query: " + query;
 document.getElementById("total").innerText = "collected("+collectionUnique+" Unique). Filtered from "+scryfallTotalResults+" total results from Scryfall ";
@@ -37,10 +37,10 @@ function setPageSize(button){
 }
 
 function updatePageSizes(){
-    var pageSizeNumbers = [15,25,50,100];//dont change the number of pageSizeNumbers
-    var pageSizes = document.getElementsByClassName("pagesize");
-    var pscounter = 0;
-    for (var i = 0; i < 4; i++){
+    let pageSizeNumbers = [15,25,50,100];//dont change the number of pageSizeNumbers
+    let pageSizes = document.getElementsByClassName("pagesize");
+    let pscounter = 0;
+    for (let i = 0; i < 4; i++){
         if (pageSizeNumbers[i] != pageSize){
             pageSizes[pscounter].innerText = pageSizeNumbers[i];     
             pscounter += 1;
@@ -56,9 +56,9 @@ function dropDownFunction(){
 function setupDropDownClicks(){
     window.onclick = function(event){
         if (!event.target.matches('.dropdownbutton')){
-            var dropdowns = document.getElementsByClassName("dropdowncontent");
-            for (var i = 0; i< dropdowns.length; i++){
-                var openDropDown = dropdowns[i];
+            let dropdowns = document.getElementsByClassName("dropdowncontent");
+            for (let i = 0; i< dropdowns.length; i++){
+                let openDropDown = dropdowns[i];
                 if (openDropDown.classList.contains('show')){
                     openDropDown.classList.remove('show');
                 }
@@ -68,15 +68,14 @@ function setupDropDownClicks(){
 }
 
 function setupImages(start,end){
-    var resultImages = document.getElementById("results");
+    let resultImages = document.getElementById("results");
     resultImages.innerHTML = '';
-    for (var i = start; i < end;i++){
-        var image = document.createElement('img');
+    for (let i = start; i < end;i++){
+        let image = document.createElement('img');
         image.src = results[i][0];
         image.style['cursor'] = "pointer";
         image.style['width']  = '20%';
-        image.onClick = function(){window.open(results[i][1]),"_blank"};
-        image.addEventListener("click",image.onClick);
+        image.addEventListener("click",function(){window.open(results[i][1]),"_blank"});
         resultImages.appendChild(image);
     }
 }
